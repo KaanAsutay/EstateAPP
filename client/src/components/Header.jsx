225661aeba2945fd4aa071787075ffed521229f9
Kaan <kaanasutay10@gmail.com>
2024-05-18 20:08:24 +0300

    create div and add in all of them

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 export default function Header() {
   return (
     <header className='bg-slate-200 shadow-md'>
+        <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
         <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
             <span className='text-slate-500'>Asutay</span>
             <span className='text-slate-700'>Estate</span>
@@ -10,6 +11,7 @@ export default function Header() {
         <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
         <input type="text" placeholder='Search...' className='bg-transparent focus:outline-none w-24 sm:w-64'/>
       </form>
+      </div>
       
     </header>
   )
