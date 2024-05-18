05690c3698795164e25c5befea7a7c427ffb16f7
Kaan <kaanasutay10@gmail.com>
2024-05-18 21:05:40 +0300

    create and run the server

--- api/index.js
+++ api/index.js
@@ -3,5 +3,5 @@ import express from 'express'
 const app = express()
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000')
+    console.log('Server is running on port 3000!')
 })
