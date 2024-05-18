6413ade2359eed555e9ec179c0ea81e69a4f8bec
Kaan <kaanasutay10@gmail.com>
2024-05-18 20:13:20 +0300

    import FaSearch

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import {FaSearch} from 'react-icons/fa'
 
 export default function Header() {
   return (
@@ -10,6 +10,7 @@ export default function Header() {
         </h1>
         <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
         <input type="text" placeholder='Search...' className='bg-transparent focus:outline-none w-24 sm:w-64'/>
+        <FaSearch className='text-slate-600'/>
       </form>
       </div>
       
