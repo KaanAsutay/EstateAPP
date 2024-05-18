8ac5e94c0c7bca1cc6091d34c3faa44d158a3718
Kaan <kaanasutay10@gmail.com>
2024-05-19 00:04:36 +0300

    create auth controller

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,5 +1,6 @@
 import express from 'express'
+import { signup } from '../controllers/auth.controller.js'
 
 const router = express.Router()
 
-router.post("/signup")
+router.post("/signup", signup)
