9e96a7e5b3ff463f0c77d0866dee96ffb9798a3f
Kaan <kaanasutay10@gmail.com>
2024-05-18 20:22:42 +0300

    add Link

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -6,6 +6,7 @@ export default function Header() {
   return (
     <header className='bg-slate-200 shadow-md'>
         <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+        <Link to='/' />
         <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
             <span className='text-slate-500'>Asutay</span>
             <span className='text-slate-700'>Estate</span>
@@ -19,9 +20,16 @@ export default function Header() {
         <FaSearch className='text-slate-600'/>
       </form>
       <ul className='flex gap-4'>
+      <Link to='/'>
+        
         <li className='hidden sm:inline text-slate-700 hover:underline'>Home</li>
+        </Link>
+        <Link to='/About'>
         <li className='hidden sm:inline text-slate-700 hover:underline'>About</li>
-        <li className='hidden sm:inline text-slate-700 hover:underline'>Sign in</li>
+        </Link>
+        <Link to='/profile'>
+        <li className='text-slate-700 hover:underline'>Sign in</li>
+        </Link>
       </ul>
       </div>
       
