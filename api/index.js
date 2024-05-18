a11aba8ba3c42e331a808b6abe1bff43e058e783
Kaan <kaanasutay10@gmail.com>
2024-05-18 22:49:54 +0300

    create router folder

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+
 dotenv.config();
 
 mongoose.connect(process.env.MONGO)
@@ -16,3 +17,5 @@ const app = express()
 app.listen(3000, () => {
     console.log('Server is running on port 3000!')
 })
+
+app.use('/api/user')
