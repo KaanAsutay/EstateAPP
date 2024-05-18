53040a072f0fff983996eeafcfdf4679a8e44485
Kaan <kaanasutay10@gmail.com>
2024-05-19 00:14:42 +0300

    user created

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -4,5 +4,7 @@ export const signup = async (req, res) => {
 
     const { username, email, password } = req.body;
     const newUser = new User({ username, email, password });
+       await newUser.save()
 
-}
+       res.status(201).json('User created successfully')
+};
