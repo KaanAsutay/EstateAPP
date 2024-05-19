cb53e18c7a01bdd2fec2023953a1b39c82d1f6e4
Kaan <kaanasutay10@gmail.com>
2024-05-19 23:10:19 +0300

    export const signIn

--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -13,5 +13,18 @@ const userSlice = createSlice({
         signInStart: (state) => {
             state.loading = true;
         },
+        signInSuccess: (state, action) => {
+            state.currentUser = action.payload;
+            state.loading = false;
+            state.error = null;
+        },
+        signInFailure: (state, action) => {
+            state.error = action.payload;
+            state.loading = false;
+        }
     }
 })
+
+export const { signInStart, signInSuccess, signInFailure } = userSlice.actions
+
+export default userSlice.reducer;
