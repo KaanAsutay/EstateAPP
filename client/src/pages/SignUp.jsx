2199f3c64469d77fc9d800cfe8804e6e4d13635d
Kaan <kaanasutay10@gmail.com>
2024-05-19 16:57:59 +0300

    create signup page and create form and h1

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -3,7 +3,28 @@ import React from 'react'
 export default function SignUp() {
   return (
     <div>
-      SignUp
+      
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
+      <form className='flex flex-col gap-4'>
+        <input
+          type='text'
+          placeholder='username'
+          className='border p-3 rounded-lg'
+          id='username'
+        />
+        <input
+          type='email'
+          placeholder='email'
+          className='border p-3 rounded-lg'
+          id='email'
+        />
+        <input
+          type='password'
+          placeholder='password'
+          className='border p-3 rounded-lg'
+          id='password'
+        />
+      </form>
     </div>
   )
 }
