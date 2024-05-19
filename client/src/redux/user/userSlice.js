5fa8ffe5be118521dc117f1bbb0a84566764cc37
Kaan <kaanasutay10@gmail.com>
2024-05-19 23:07:36 +0300

    create user folder and add redux toolkit

---
+++ client/src/redux/user/userSlice.js
@@ -0,0 +1,17 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    currentUser: null,
+    error: null,
+    loading: false
+}
+
+const userSlice = createSlice({
+    name: 'user',
+    initialState,
+    reducers: {
+        signInStart: (state) => {
+            state.loading = true;
+        },
+    }
+})
