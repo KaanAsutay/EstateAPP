59723d337d02372c33efcd9648554f4656bfabdd
Kaan <kaanasutay10@gmail.com>
2024-05-20 14:58:58 +0300

    add firebase storage

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,18 +1,32 @@
 import {useSelector} from 'react-redux'
-
+import { useRef } from 'react'
 
 export default function Profile() {
 
+  const fileRef = useRef(null)
   const {currentUser} = useSelector((state) => state.user)
 
-
+  // firebase storage
+      // allow read;
+      // allow write: if
+      // request.resource.size < 2 * 1024 * 1024 &&
+      // request.resource.contentType.matches('image/.*')
 
 
   return (
     <div className='p-3 max-w-lg mx-auto'>
         <h1 className="text3xl font-semibold text-center my-7"> Profile </h1>
         <form className='flex flex-col gap-4'>
-        <img 
+
+        <input 
+          type='file' 
+          ref={fileRef} 
+          hidden 
+          accept='image/*' 
+        />
+
+        <img
+          onClick={() => fileRef.current.click()} 
           src={currentUser.avatar} 
           alt="profile" 
           className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2' 
