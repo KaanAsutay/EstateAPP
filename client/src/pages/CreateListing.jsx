5c28aaa82cc18c9c663badbcd538d925d6c3ddca
Kaan <kaanasutay10@gmail.com>
2024-05-27 23:06:56 +0300

    create images upload

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -4,7 +4,7 @@ export default function CreateListing() {
   return (
     <main className='p-3 max-w-4xl mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Create a Listing</h1>
-      <form className='flex flex-col sm:flex-row'>
+      <form className='flex flex-col sm:flex-row gap-4'>
 
         <div className="flex flex-col gap-4 flex-1">
 
@@ -66,17 +66,38 @@ export default function CreateListing() {
             <div className='flex flex-wrap gap-6'>
 
                 <div className='flex items-center gap-2'>
-                    <input type="number" id='bedrooms' min='1' max='10' required className='p-3 border border-gray-300 rounded-lg'/>
+                    <input 
+                    type="number" 
+                    id='bedrooms' 
+                    min='1' 
+                    max='10' 
+                    required 
+                    className='p-3 border border-gray-300 rounded-lg'
+                    />
                     <p>Beds</p>
                 </div>
 
                 <div className='flex items-center gap-2'>
-                    <input type="number" id='bathrooms' min='1' max='10' required className='p-3 border border-gray-300 rounded-lg'/>
+                    <input 
+                    type="number" 
+                    id='bathrooms' 
+                    min='1' 
+                    max='10' 
+                    required 
+                    className='p-3 border border-gray-300 rounded-lg'
+                    />
                     <p>Baths</p>
                 </div>
 
                 <div className='flex items-center gap-2'>
-                    <input type="number" id='regularPrice' min='1' max='10' required className='p-3 border border-gray-300 rounded-lg'/>
+                    <input 
+                    type="number" 
+                    id='regularPrice' 
+                    min='1' 
+                    max='10' 
+                    required 
+                    className='p-3 border border-gray-300 rounded-lg'
+                    />
                     <div className='flex flex-col items-center'>
                     <p>Regular price</p>
                     <span className='text-xs'> ($ / Month) </span>
@@ -84,7 +105,14 @@ export default function CreateListing() {
                 </div>
 
                 <div className='flex items-center gap-2'>
-                    <input type="number" id='discountPrice' min='1' max='10' required className='p-3 border border-gray-300 rounded-lg'/>
+                    <input 
+                    type="number" 
+                    id='discountPrice' 
+                    min='1' 
+                    max='10' 
+                    required 
+                    className='p-3 border border-gray-300 rounded-lg'
+                    />
                     <div className='flex flex-col items-center'>
                     <p>Discounted price</p>
                     <span className='text-xs'> ($ / Month) </span>
@@ -94,6 +122,15 @@ export default function CreateListing() {
             </div>
 
         </div>
+
+        <div className='flex flex-col flex-1 gap-4'>
+            <p className='font-semibold'>Images: <span className='font-normal text-gray-600 ml-2'>The first image will be the cover (max 6) </span></p>
+            <div className='flex gap-4'>
+                <input className='p-3 border border-gray-300 rounded w-full' type="file" id='images' accept='image/*' multiple />
+                <button className='p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80'>Upload</button>
+            </div>
+        </div>
+
       </form> 
     </main>
   )
