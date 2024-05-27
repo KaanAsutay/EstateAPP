5bd34486a9fc77d96c163615d378edf18abf8c3c
Kaan <kaanasutay10@gmail.com>
2024-05-28 01:08:48 +0300

    const storageImage

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -1,8 +1,42 @@
 import { useState } from "react"
+import { getDownloadURL, getStorage, uploadBytesResumable } from 'firebase/storage'
+import { app } from '../firebase'
 
 export default function CreateListing() {
 
     const [files, setFiles] = useState([])
+
+
+    const handleImageSubmit = (e) => {
+        if (files.length > 0 && files.length < 7) {
+        const promises = [];
+
+        for (let i = 0; i < files.length; i++) {
+            promises.push(storeImage(files[1]));
+        }
+
+    }
+}
+
+    const storeImage = async (file) => {
+        return new Promise((resolve, reject) => {
+            const storage = getStorage(app);
+            const fileName = new Date().getTime() + file.name
+            const storageRef = ref(storage, fileName)
+            const uploadTask = uploadBytesResumable(storageRef, file)
+            uploadTask.on(
+                "state_changed",
+                (error) => {
+                    reject(error)
+                },
+                () => {
+                    getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                        resolve(downloadURL)
+                    })
+                }
+            )
+        })
+    }
 
     
   return (
@@ -138,7 +172,12 @@ export default function CreateListing() {
                 accept='image/*' 
                 multiple 
                 />
-                <button className='p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80'>Upload</button>
+                <button
+                type="button" 
+                onClick={handleImageSubmit} 
+                className='p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80'>
+                Upload
+                </button>
             </div>
             <button className='p-3 bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>Create Listing</button>
         </div>
