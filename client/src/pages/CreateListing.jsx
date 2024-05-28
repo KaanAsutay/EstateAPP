84798f1a2fd9dc6843e77b71ab381a788789ebdd
Kaan <kaanasutay10@gmail.com>
2024-05-28 21:17:58 +0300

    create const handleChange

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -84,13 +84,45 @@ export default function CreateListing() {
         });
       };
 
-      const handleChange = (e) => {};
+      const handleChange = (e) => {
+        if (
+          e.target.id === 'sale' || 
+          e.target.id === 'rent'
+        ) {
+          setFormData({
+            ...formData,
+            type: e.target.id
+          })
+        }
+
+        if (
+          e.target.id === 'parking' || 
+          e.target.id === 'furnished' || 
+          e.target.id === 'offer'
+        ) {
+          setFormData({
+            ...formData,
+            [e.target.id]: e.target.checked
+          })
+        }
+
+        if (
+          e.target.type === 'number' || 
+          e.target.type === 'text' || 
+          e.target.type === 'textarea'
+        ) {
+          setFormData({
+            ...formData,
+            [e.target.id]: e.target.value
+          })
+        }
+      };
 
     
   return (
     <main className='p-3 max-w-4xl mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Create a Listing</h1>
-      <form className='flex flex-col sm:flex-row gap-4'>
+      <form onSubmit={handleSubmit} className='flex flex-col sm:flex-row gap-4'>
 
         <div className="flex flex-col gap-4 flex-1">
 
