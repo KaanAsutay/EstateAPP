ec13fb624d457b3c5067ec9f9765000fc720e136
Kaan <kaanasutay10@gmail.com>
2024-05-28 22:47:01 +0300

    create loading and uploading

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -374,7 +374,8 @@ export default function CreateListing() {
                 </button>
               </div>
             ))}
-            <button 
+            <button
+            disabled={loading || uploading} 
             className='p-3 bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>
             {loading ? 'Creating...' : 'Create listing'}
             </button>
