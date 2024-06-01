35cd90e5e8063755d31a27670f04485c7e221641
Kaan <kaanasutay10@gmail.com>
2024-06-02 01:07:25 +0300

    Complete show user listings functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -17,6 +17,7 @@ export default function Profile() {
   const [formData, setFormData] = useState({})
   const [updateSuccess, setUpdateSuccess] = useState(false)
   const [showListingsError, setShowListingsError] = useState(false)
+  const [userListings, setUserListings] = useState([])
   const dispatch = useDispatch()
 
       // firebase storage
@@ -118,9 +119,17 @@ export default function Profile() {
     const handleShowListings = async () => {
 
       try {
-        
+        setShowListingsError(false)
+        const res = await fetch(`/api/user/listings/${currentUser._id}`)
+        const data = await res.json();
+        if (data.success === false) {
+          setShowListingsError(true);
+          return;
+        }
+
+        setUserListings(data);
       } catch (error) {
-        
+        setShowListingsError(true)
       }
 
     }
@@ -204,7 +213,36 @@ export default function Profile() {
 
       <button onClick={handleShowListings} className='text-green-700 w-full'>Show Listings</button>
       <p className='text-red-700 mt-5'> {showListingsError ? 'Error showing listings': ''} </p>
+      
+      {userListings && 
+        userListings.length > 0 &&
+        <div className="flex flex-col gap-4">
+          <h1 className='text-center mt-7 text-2xl font-semibold'>Your Listings</h1>
+           {userListings.map((listing) => (
+        <div 
+          key={listing._id} 
+          className="border rounded-lg p-3 flex justify-between items-center gap-4"
+        >
+          <Link to={`/listing/${listing._id}`}>
+            <img 
+            src={listing.imageUrls[0]} 
+            alt="listing cover" 
+            className='h-16 w-16 object-contain'
+            />
+          </Link>
 
+          <Link className='text-slate-700 font-semibold hover:underline truncate flex-1' to={`/listing/${listing._id}`}>
+            <p>{listing.name}</p>
+          </Link>
+
+          <div className='flex flex-col items-center'>
+            <button className='text-red-700 uppercase'>Delete</button>
+            <button className='text-green-700 uppercase'>Edit</button>
+          </div>
+        </div>
+      ))}
+        </div>}
+       
     </div>
   );
 }
