70a6d7ee204b17d1dfd33a0a0e26efd93180f76a
Kaan <kaanasutay10@gmail.com>
2024-06-05 00:16:23 +0300

    complete delete user listing functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -114,7 +114,7 @@ export default function Profile() {
       } catch (error) {
         dispatch(signOutUserFailure(data.message))
       }
-    }
+    };
 
     const handleShowListings = async () => {
 
@@ -132,7 +132,25 @@ export default function Profile() {
         setShowListingsError(true)
       }
 
-    }
+    };
+
+    const handleListingDelete = async (listingId) => {
+      try {
+        const res = await fetch(`/api/listing/delete/${listingId}`, {
+          method: 'DELETE'
+        })
+        const data = await res.json();
+        if (data.success === false) {
+          console.log(data.message)
+          return;
+        }
+
+        setUserListings((prev) => 
+          prev.filter((listing) => listing._id !== listingId));
+      } catch (error) {
+        console.log(error.message)
+      }
+    };
 
   return (
     <div className='p-3 max-w-lg mx-auto'>
@@ -236,7 +254,7 @@ export default function Profile() {
           </Link>
 
           <div className='flex flex-col items-center'>
-            <button className='text-red-700 uppercase'>Delete</button>
+            <button onClick={() => handleListingDelete(listing._id)} className='text-red-700 uppercase'>Delete</button>
             <button className='text-green-700 uppercase'>Edit</button>
           </div>
         </div>
