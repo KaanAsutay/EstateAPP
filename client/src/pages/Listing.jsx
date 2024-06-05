1c147d6e2367dfe0e89c92d6b079d3b174f7b9e0
Kaan <kaanasutay10@gmail.com>
2024-06-05 19:07:26 +0300

    add image slider to the listing page

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -10,7 +10,6 @@ export default function Listing() {
     const [listing, setListing] = useState(null)
     const [loading, setLoading] = useState(false)
     const [error, setError] = useState(false)
-
     const params = useParams()
 
     useEffect(() => {
@@ -36,12 +35,29 @@ export default function Listing() {
     }, [params.listingId])
 
 
-  return (
-    <main> 
-        {loading && <p className="text-center my-7 text-2xl">Loading...</p>}
-        {error && <p className="text-center my-7 text-2xl">Something went wrong!</p>}
-        
-    
+    return (
+        <main>
+          {loading && <p className='text-center my-7 text-2xl'>Loading...</p>}
+          {error && (
+            <p className='text-center my-7 text-2xl'>Something went wrong!</p>
+          )}
+          {listing && !loading && !error && (
+            <div>
+              <Swiper navigation>
+                {listing.imageUrls.map((url) => (
+                  <SwiperSlide key={url}>
+                    <div
+                      className='h-[550px]'
+                      style={{
+                        background: `url(${url}) center no-repeat`,
+                        backgroundSize: 'cover',
+                      }}
+                    ></div>
+                  </SwiperSlide>
+                ))}
+              </Swiper>
+        </div>
+        )}
     </main>
   )
 }
