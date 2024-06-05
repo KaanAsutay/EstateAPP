3b7e4595cfa8ff5286f3e33aa637e0bf9b59ef9c
Kaan <kaanasutay10@gmail.com>
2024-06-05 16:50:59 +0300

    create useEffect its important

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
 
 export default function Listing() {
+    const [listing, setListing] = useState(null)
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState(false)
+
+    const params = useParams()
+
+    useEffect(() => {
+        const fetchListing = async () => {
+            try {
+                setLoading(true)
+            const res = await fetch(`/api/listing/get/${params.listingId}`);
+            const data = await res.json()
+                if (data.success === false) {
+                    setError(true)
+                    setLoading(false)
+                    return;
+                }
+                setListing(data)
+            } catch (error) {
+                setError(true)
+                setLoading(false)
+            }
+        }
+        fetchListing()
+    })
+
+
   return (
-    <div>
-      listing
-    </div>
+    <main> {listing && listing.name} </main>
   )
 }
