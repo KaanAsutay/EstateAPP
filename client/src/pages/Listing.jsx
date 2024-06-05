2940ee8ca8d3130c18447c81994b2ac5ad47b11c
Kaan <kaanasutay10@gmail.com>
2024-06-05 21:46:44 +0300

    add currentUser

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -14,7 +14,6 @@ import {
   FaParking,
   FaShare,
 } from 'react-icons/fa';
-// import Contact from '../components/Contact';
 
 // https://sabe.io/blog/javascript-format-numbers-commas#:~:text=The%20best%20way%20to%20format,format%20the%20number%20with%20commas.
 
@@ -24,7 +23,6 @@ export default function Listing() {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
   const [copied, setCopied] = useState(false);
-  const [contact, setContact] = useState(false);
   const params = useParams();
   const { currentUser } = useSelector((state) => state.user);
 
@@ -136,6 +134,9 @@ export default function Listing() {
                 {listing.furnished ? 'Furnished' : 'Unfurnished'}
               </li>
             </ul>
+            {currentUser && listing.userRef !== currentUser._id && (
+                <button className='bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 p-3'>Contact landlord</button>
+            )}
           </div>
         </div>
       )}
