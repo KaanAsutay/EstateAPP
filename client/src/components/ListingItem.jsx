a79bbf840a48cacae7c8801935b2a81c4313d980
Kaan <kaanasutay10@gmail.com>
2024-06-07 01:47:06 +0300

    add images

--- client/src/components/ListingItem.jsx
+++ client/src/components/ListingItem.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function ListingItem({ listing }) {
   return (
-    <div>{listing.name}</div>
-  )
+    <div className='bg-white shadow-md hover:shadow-lg transition-shadow overflow-hidden rounded-lg'>
+        <Link to={`/listing/${listing._id}`}>
+            <img 
+            src={listing.imageUrls[0]} 
+            alt="listing cover" 
+            className='h-[320px] sm:h-[220px] w-full object-cover hover:scale-105 transition-scale duration-300' 
+            />
+            <div className="p-3">
+                <p className='truncate text-lg font-semibold text-slate-700'>{listing.name}</p>
+            </div>
+        </Link>
+    </div>
+  );
 }
