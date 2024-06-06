3456461d6d11802b35d9572bbc8d92c45ec82cca
Kaan <kaanasutay10@gmail.com>
2024-06-07 02:12:21 +0300

    add bathrooms and beds

--- client/src/components/ListingItem.jsx
+++ client/src/components/ListingItem.jsx
@@ -14,9 +14,24 @@ export default function ListingItem({ listing }) {
                 <p className='truncate text-lg font-semibold text-slate-700'>{listing.name}</p>
                 <div className="flex items-center gap-1">
                     <MdLocationOn className='h-4 w-4 text-green-700'/>
-                    <p className='text-sm text-gray-600 truncate'>{listing.address}</p>
+                    <p className='text-sm text-gray-600 truncate w-full'>{listing.address}</p>
                 </div>
-                
+                    <p className='text-sm text-gray-600 line-clamp-2'>{listing.description}</p>
+                    <p className='text-slate-500 mt-2 font-semibold'> 
+                        $ 
+                        {listing.offer ? 
+                        listing.discountPrice.toLocaleString('en-US') 
+                        : listing.regularPrice.toLocaleString('en-US')}
+                        {listing.type === 'rent' && ' / month'}
+                    </p>
+                <div className="text-slate-700 flex gap-4">
+                    <div className="font-bold text-xs">
+                        {listing.bedrooms > 1 ? `${listing.bedrooms} beds` : `${listing.bedrooms} bed `}
+                    </div>
+                    <div className="font-bold text-xs">
+                        {listing.bathrooms > 1 ? `${listing.bathrooms} baths` : `${listing.bathrooms} bath `}
+                    </div>
+                </div>
             </div>
         </Link>
     </div>
