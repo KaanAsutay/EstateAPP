272f91ec0b670af95c1a6c35c06fbf96ee23b573
Kaan <kaanasutay10@gmail.com>
2024-06-07 02:18:57 +0300

    everything OK

--- client/src/components/ListingItem.jsx
+++ client/src/components/ListingItem.jsx
@@ -5,11 +5,14 @@ export default function ListingItem({ listing }) {
   return (
     <div className='bg-white shadow-md hover:shadow-lg transition-shadow overflow-hidden rounded-lg w-full sm:w-[330px]'>
         <Link to={`/listing/${listing._id}`}>
-            <img 
-            src={listing.imageUrls[0]} 
-            alt="listing cover" 
-            className='h-[320px] sm:h-[220px] w-full object-cover hover:scale-105 transition-scale duration-300' 
-            />
+                 <img
+                    src={
+                        listing.imageUrls[0] ||
+                        'https://53.fs1.hubspotusercontent-na1.net/hub/53/hubfs/Sales_Blog/real-estate-business-compressor.jpg?width=595&height=400&name=real-estate-business-compressor.jpg'
+                        }
+                        alt='listing cover'
+                        className='h-[320px] sm:h-[220px] w-full object-cover hover:scale-105 transition-scale duration-300'
+                        />
             <div className="p-3 flex flex-col gap-2 w-full">
                 <p className='truncate text-lg font-semibold text-slate-700'>{listing.name}</p>
                 <div className="flex items-center gap-1">
