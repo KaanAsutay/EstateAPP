332463ee4a05cd78e7671586b41df3ea72028ab0
Kaan <kaanasutay10@gmail.com>
2024-06-06 22:00:02 +0300

    created and add handleChange

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,6 +1,18 @@
-import React from 'react'
+import { useState } from 'react'
 
 export default function Search() {
+    const [sidebardata, setSidebardata] = useState({
+        searchTerm: '',
+        type: 'all',
+        parking: false,
+        furnished: false,
+        offer: false,
+        sort: 'created_at',
+        order: 'desc'
+    });
+
+    const handleChange = (e) => {};
+
   return (
     <div className='flex flex-col md:flex-row'>
         <div className="p-7 border-b-2 md:border-r-2 md:min-h-screen">
@@ -12,7 +24,9 @@ export default function Search() {
                     type="text" 
                     id='searchTerm' 
                     placeholder='Search...' 
-                    className='border rounded-lg p-3 w-full' 
+                    className='border rounded-lg p-3 w-full'
+                    value={sidebardata.searchTerm}
+                    onChange={handleChange} 
                     />
                 </div>
 
@@ -24,6 +38,8 @@ export default function Search() {
                         type="checkbox" 
                         id="all" 
                         className='w-5'
+                        onChange={handleChange}
+                        checked={sidebardata.type === 'all'}
                          />
                         <span>Rent & Sale</span>
                     </div>
@@ -33,6 +49,8 @@ export default function Search() {
                         type="checkbox" 
                         id="rent" 
                         className='w-5'
+                        onChange={handleChange}
+                        checked={sidebardata.type === 'rent'}
                         />
                         <span>Rent</span>
                     </div>
@@ -41,7 +59,9 @@ export default function Search() {
                         <input 
                         type="checkbox" 
                         id="sale" 
-                        className='w-5' 
+                        className='w-5'
+                        onChange={handleChange}
+                        checked={sidebardata.type === 'sale'} 
                         />
                         <span>Sale</span>
                     </div>
@@ -50,7 +70,9 @@ export default function Search() {
                         <input 
                         type="checkbox" 
                         id="offer" 
-                        className='w-5' 
+                        className='w-5'
+                        onChange={handleChange}
+                        checked={sidebardata.offer} 
                         />
                         <span>Offer</span>
                     </div>
@@ -65,6 +87,8 @@ export default function Search() {
                         type="checkbox" 
                         id="parking" 
                         className='w-5'
+                        onChange={handleChange}
+                        checked={sidebardata.parking}
                          />
                         <span>Parking</span>
                     </div>
@@ -74,6 +98,8 @@ export default function Search() {
                         type="checkbox" 
                         id="furnished" 
                         className='w-5'
+                        onChange={handleChange}
+                        checked={sidebardata.furnished}
                         />
                         <span>Furnished</span>
                     </div>
@@ -82,11 +108,16 @@ export default function Search() {
 
                 <div className="">
                     <label className='font-semibold'>Sort:</label>
-                    <select id="sort_order" className='border rounded-lg p-3'>
-                        <option>Price high to low</option>
-                        <option>Price low to high</option>
-                        <option>Latest</option>
-                        <option>Oldest</option>
+                    <select 
+                    id="sort_order" 
+                    className='border rounded-lg p-3'
+                    onChange={handleChange}
+                    defaultValue={'created_at_desc'}
+                    >
+                        <option value='regularPrice_desc'>Price high to low</option>
+                        <option value='regularPrice_asc'>Price low to high</option>
+                        <option value='createdAt_desc'>Latest</option>
+                        <option value='createdAt_asc'>Oldest</option>
                     </select>
                 </div>
 
