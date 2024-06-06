a481ec2a04366201f71eec6f37ededbe31edc04e
Kaan <kaanasutay10@gmail.com>
2024-06-06 23:21:19 +0300

    create useEEfect

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 export default function Search() {
@@ -13,6 +13,37 @@ export default function Search() {
         sort: 'created_at',
         order: 'desc'
     });
+
+    useEffect(() => {
+        const urlParams = new URLSearchParams(location.search);
+        const searchTermFromUrl = urlParams.get('searchTerm');
+        const typeFromUrl = urlParams.get('type');
+        const parkingFromUrl = urlParams.get('parking');
+        const furnishedFromUrl = urlParams.get('furnished');
+        const offerFromUrl = urlParams.get('offer');
+        const sortFromUrl = urlParams.get('sort');
+        const orderFromUrl = urlParams.get('order');
+    
+        if (
+          searchTermFromUrl ||
+          typeFromUrl       ||
+          parkingFromUrl    ||
+          furnishedFromUrl  ||
+          offerFromUrl      ||
+          sortFromUrl       ||
+          orderFromUrl
+        ) {
+          setSidebardata({
+            searchTerm: searchTermFromUrl || '',
+            type: typeFromUrl || 'all',
+            parking: parkingFromUrl === 'true' ? true : false,
+            furnished: furnishedFromUrl === 'true' ? true : false,
+            offer: offerFromUrl === 'true' ? true : false,
+            sort: sortFromUrl || 'created_at',
+            order: orderFromUrl || 'desc',
+          });
+        }
+    }, [location.search])
 
     const handleChange = (e) => {
 
