3fa1616193dbd773685ab326e427ef0417c5cec0
Kaan <kaanasutay10@gmail.com>
2024-06-08 00:27:01 +0300

    add rentListings

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -99,6 +99,7 @@ export default function Home() {
       {/* listing results for offer, sale and rent */}
 
       <div className='max-w-6xl mx-auto p-3 flex flex-col gap-8 my-10'>
+
         {offerListings && offerListings.length > 0 && (
           <div className=''>
             <div className='my-3'>
@@ -112,6 +113,22 @@ export default function Home() {
             </div>
           </div>
         )}
+
+        {rentListings && rentListings.length > 0 && (
+          <div className=''>
+            <div className='my-3'>
+              <h2 className='text-2xl font-semibold text-slate-600'>Recent places for rent</h2>
+              <Link className='text-sm text-blue-800 hover:underline' to={'/search?type=rent'}>Show more places for rent</Link>
+            </div>
+            <div className='flex flex-wrap gap-4'>
+              {rentListings.map((listing) => (
+                <ListingItem listing={listing} key={listing._id} />
+              ))}
+            </div>
+          </div>
+        )}
+
+
       </div>
 
 
