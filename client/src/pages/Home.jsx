f4f5708c5971e6bde405549b31bec63d653300bb
Kaan <kaanasutay10@gmail.com>
2024-06-08 00:09:48 +0300

    create and connect useEffect

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,56 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 export default function Home() {
+  const [offerListings, setOfferListings] = useState([]);
+  const [saleListings, setSaleListings] = useState([]);
+  const [rentListings, setRentListings] = useState([]);
+  console.log(saleListings)
+
+  useEffect(() => {
+
+    const fetchOfferListings = async () => {
+      try {
+        const res = await fetch('/api/listing/get?offer=true&limit=4');
+        const data = await res.json();
+        setOfferListings(data);
+        fetchRentListings();
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    const fetchRentListings = async () => {
+      try {
+        const res = await fetch('/api/listing/get?type=rent&limit=4');
+        const data = await res.json();
+        setRentListings(data);
+        fetchSaleListings();
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    const fetchSaleListings = async () => {
+      try {
+        const res = await fetch('/api/listing/get?type=sale&limit=4');
+        const data = await res.json();
+        setSaleListings(data);
+      } catch (error) {
+        log(error);
+      }
+    };
+
+    fetchOfferListings()
+  }, [])
+
+
+
   return (
     <div>
 
       {/* top */}
+
         <div className="flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto">
           <h1 className='text-slate-700 font-bold text-3xl lg:text-6xl'>
             Find your next  
@@ -26,6 +72,8 @@ export default function Home() {
         </div>
 
       {/* swiper */}
+
+
 
 
       {/* listing results for offer, sale and rent */}
