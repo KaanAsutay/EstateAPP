888cb9f9253f937707f4914370b8e92d72107b11
Kaan <kaanasutay10@gmail.com>
2024-06-07 23:50:58 +0300

    add link

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -27,12 +27,13 @@ export default function Header() {
   return (
     <header className='bg-slate-200 shadow-md'>
         <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
-        <Link to='/' />
-        
+          
+        <Link to='/'>
         <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
             <span className='text-slate-500'>Asutay</span>
             <span className='text-slate-700'>Estate</span>
         </h1>
+        </Link>
 
         <form onSubmit={handleSubmit} className='bg-slate-100 p-3 rounded-lg flex items-center'>
             <input 
