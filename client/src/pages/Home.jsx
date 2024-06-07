f6f5811ea92b1b552fc0acf7f98b886a08259591
Kaan <kaanasutay10@gmail.com>
2024-06-08 00:00:18 +0300

    create top section

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import { Link } from 'react-router-dom';
 
 export default function Home() {
   return (
     <div>
 
       {/* top */}
-
+        <div className="flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto">
+          <h1 className='text-slate-700 font-bold text-3xl lg:text-6xl'>
+            Find your next  
+            <span className='text-slate-500'> perfect</span> 
+            <br /> 
+            place with ease
+          </h1>
+          <div className='text-gray-400 text-xs sm:text-sm'>
+          Asutay Estate is the best place to find your next perfect place to live.
+          <br />
+          We have a wide range of properties for you to choose from.
+        </div>
+        <Link 
+        className='text-xs sm:text-sm text-blue-800 font-bold hover:underline' 
+        to={"/search"}
+        >
+        Let's get started...
+        </Link>
+        </div>
 
       {/* swiper */}
 
